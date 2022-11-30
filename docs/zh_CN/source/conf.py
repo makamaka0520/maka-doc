f6b4f28b12e662f3763db424f394756ba44e8d7e
TuTuHuss <74552232+TuTuHuss@users.noreply.github.com>
2022-11-30 16:32:31 +0800

    Update conf.py

--- docs/zh_CN/source/conf.py
+++ docs/zh_CN/source/conf.py
@@ -35,7 +35,7 @@ extensions = [
 ]
 
 # multi-language docs
-language = 'zh_CN'
+#language = 'zh_CN'
 locale_dirs = ['./']   # path is example but recommended.
 gettext_compact = False  # optional.
 gettext_uuid = True  # optional.
