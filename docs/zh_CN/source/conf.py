dda8c8479db849c538935693c3916a689c3822e1
TuTuHuss <74552232+TuTuHuss@users.noreply.github.com>
2022-12-05 03:57:53 +0800

    Update conf.py

--- docs/zh_CN/source/conf.py
+++ docs/zh_CN/source/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'mkbk-doc'
-copyright = '2022, mkbk'
-author = 'mkbk'
+project = 'bkmk-doc-zh_CN'
+copyright = '2022, bkmk'
+author = 'bkmk'
 
 # The full version, including alpha/beta/rc tags
 release = 'v0.0.1'
